cc22376e0a103d4f802f474502cb2989b7e92334
Pokernab <53443894+Pokernab@users.noreply.github.com>
2019-08-27 16:36:38 +0300

    X

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
